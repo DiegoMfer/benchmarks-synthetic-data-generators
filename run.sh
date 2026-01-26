50d92fd82a3bb18aff1159cfe2f0adc54e11d056
DiegoMfer <diegomartin.research@gmail.com>
2026-01-26 18:46:17 +0000

    run changes

--- run.sh
+++ run.sh
@@ -5,13 +5,11 @@ echo "Starting generation of ALL datasets..."
 echo "Generators: BSBM, LUBM, GAIA, LINKGEN, PYGRAFT, RDFGRAPHGEN, RUDOFGENERATE, and LUBM Variants"
 
 # Run 4 times as requested
-for i in {1..4}
-do
-    echo "----------------------------------------------------------------"
-    echo "Starting Iteration $i/4"
-    echo "----------------------------------------------------------------"
-    python3 generate_all_datasets.py --generators ALL
-done
+
+echo "----------------------------------------------------------------"
+echo "Starting generation"
+echo "----------------------------------------------------------------"
+python3 generate_all_datasets.py --generators ALL
 
 echo "Generating CSV metrics..."
 python3 generate_csv_metrics.py
